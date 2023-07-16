acfd5f90478fb8e8ac59ec69dd7cd0ca28d6f829
Benjamin <beniagic@gmail.com>
2023-07-16 19:58:57 +0200

    CsvModal

--- src/features/modal/CsvModal.js
+++ src/features/modal/CsvModal.js
@@ -2,23 +2,90 @@ import React, { useState, useEffect } from "react";
 import { selectCurrentToken } from "../../features/auth/authSlice";
 import useAxiosInstance from "../../utils/axiosInstance";
 import { useSelector } from "react-redux";
+import Papa from "papaparse";
+import { useNavigate, useParams } from "react-router-dom";
 
 const CsvModal = ({ showModal, onClose }) => {
   const [show, setShowModal] = useState(showModal);
-  const [columns, setColumns] = useState([]);
+  const allowedExtensions = ["csv"];
+  const [file, setFile] = useState("");
+  const [error, setError] = useState("");
+  const params = useParams();
+  const axiosInstance = useAxiosInstance();
+  const token = useSelector(selectCurrentToken);
   useEffect(() => {
     setShowModal(showModal);
   }, [showModal]);
 
   const handleCsvFile = async (e) => {
-    // const reader = new FileReader();
-    // reader.onload = async ({ target }) => {
-    //   const csv = Papa.parse(target.result, { header: true });
-    //   const parsedData = csv?.data;
-    //   const columns = Object.keys(parsedData[0]);
-    //   setColumns(columns);
-    // };
-    // reader.readAsText(file);
+    setError("");
+
+    // Check if user has entered the file
+    if (e.target.files.length) {
+      const inputFile = e.target.files[0];
+
+      // Check the file extensions, if it not
+      // included in the allowed extensions
+      // we show the error
+      const fileExtension = inputFile?.type.split("/")[1];
+      if (!allowedExtensions.includes(fileExtension)) {
+        setError("Please input a csv file");
+        return;
+      }
+
+      // If input type is correct set the state
+      setFile(inputFile);
+    }
+  };
+
+  const handleParse = async () => {
+    // If user clicks the parse button without
+    // a file we show a error
+    if (!file) return setError("Enter a valid file");
+
+    // Initialize a reader which allows user
+    // to read any file or blob.
+    const reader = new FileReader();
+
+    // Event listener on reader when the file
+    // loads, we parse it and set the data.
+    reader.onload = async ({ target }) => {
+      const csv = Papa.parse(target.result, {
+        header: true,
+        skipEmptyLines: true,
+      });
+      const parsedData = csv?.data;
+
+      await createContact(parsedData);
+    };
+    reader.readAsText(file);
+    closeModal();
+  };
+
+  const createContact = async (parsedData) => {
+    for (const contact of parsedData) {
+      try {
+        let response = await axiosInstance.post(
+          `http://127.0.0.1:8000/api/create_contact/${params.id}/`,
+          {
+            first_name: contact.first_name,
+            last_name: contact.last_name,
+            phone_number: contact.phone_number,
+            email: contact.email,
+            contact_list: params.id,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + String(token),
+            },
+          }
+        );
+        console.log("Response:", response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   const closeModal = () => {
@@ -61,7 +128,7 @@ const CsvModal = ({ showModal, onClose }) => {
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={closeModal}
+                    onClick={handleParse}
                   >
                     Save Changes
                   </button>
