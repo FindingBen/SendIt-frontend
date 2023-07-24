ab3c41be4f64974c60009165366159abd256e324
Benjamin <beniagic@gmail.com>
2023-07-24 16:50:13 +0200

    Change

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials, logOut } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   //baseUrl: "http://localhost:8000/",
-  baseUrl: "https://sendit-backend-production.up.railway.app/"
+  baseUrl: "https://sendit-backend-production.up.railway.app/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
