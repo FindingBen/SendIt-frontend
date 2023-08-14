066270bf0390191387aac6c86d6e3b418f701264
FindingBen <beniagic@gmail.com>
2023-08-14 16:18:11 +0200

    Commit

--- src/pages/SmsEditor.js
+++ src/pages/SmsEditor.js
@@ -51,14 +51,14 @@ const SmsEditor = () => {
   }, [smsText, textComponentRef]);
 
   const handleAddLink = (e) => {
-    const trackingUUID = uuidv4()
+    //const trackingUUID = uuidv4()
     const linkEmbed = `#Link`;
     const textarea = document.getElementById("smsTextArea");
     const startPos = textarea.selectionStart;
     const endPos = textarea.selectionEnd;
 
     setLinkURL(linkURLBase);
-    <TrackLink trackingUrl={trackingUUID} destinationUrl={linkURLBase}/>
+    //<TrackLink trackingUrl={trackingUUID} destinationUrl={linkURLBase}/>
     // Insert the link at the specified index
     setSmsText(
       smsText.substring(0, startPos) + linkEmbed + smsText.substring(endPos)
@@ -69,40 +69,6 @@ const SmsEditor = () => {
     setSmsText(e.target.value);
   };
 
-  // let getUser = async () => {
-  //   try {
-  //     let response = await axiosInstance.get(`/api/user_account/${userId}/`, {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: "Bearer " + String(token),
-  //       },
-  //     });
-
-  //     if (response.status === 200) {
-  //       setUser(response.data);
-  //     } else {
-  //       localStorage.removeItem("tokens");
-  //       navigate("/login");
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // const getMessage = async () => {
-  //   try {
-  //     const response = await axiosInstance.get(`/sms/sms-editor/${params.id}`);
-
-  //     if (response.status === 200) {
-  //       setMessage(response.data);
-  //     } else if (response.statusText === "Unauthorized") {
-  //       dispatch(logOut());
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
 
   const sendSms = async (e) => {
     e.preventDefault();
@@ -219,14 +185,14 @@ const SmsEditor = () => {
               )}
             </div>
             <div className="col">
-              <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-xl ">
+              <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-500 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-xl ">
                 <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 absolute"></div>
                 <div className="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[124px] rounded-l-lg"></div>
                 <div className="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
                 <div className="h-[64px] w-[3px] bg-gray-800 absolute -right-[17px] top-[142px] rounded-r-lg"></div>
 
                 <div
-                  className="rounded-[2rem] overflow-hidden w-[270px] h-[572px] bg-gray-950 dark:bg-gray-800"
+                  className="rounded-[2rem] overflow-hidden w-[270px] h-[572px] bg-gray-500 dark:bg-gray-800"
                   id="screen"
                 >
                   <div className="yours message" style={{ marginTop: "25px" }}>
