aaaa79314ef48b34db0e115b2ab4ac84bd68e932
Benjamin <beniagic@gmail.com>
2023-10-20 19:06:03 +0200

    comit

--- src/pages/SmsEditor.js
+++ src/pages/SmsEditor.js
@@ -25,7 +25,7 @@ const SmsEditor = () => {
   const [show, setShow] = useState(false);
   const [showSchedule, setShowSchedule] = useState(false);
   const [linkURL, setLinkURL] = useState("");
-  const [dateSchedule, setDateSchedule] = useState("2023-10-17 11:49:00");
+  const [dateSchedule, setDateSchedule] = useState("2023-10-17 19:12:00");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [smsText, setSmsText] = useState([]);
