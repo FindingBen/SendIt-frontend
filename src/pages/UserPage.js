090ce0195b62d7046329cb17cfe668acd0b572b3
Benjamin <beniagic@gmail.com>
2023-10-23 20:03:35 +0200

    commit

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -121,7 +121,7 @@ const UserPage = () => {
       setCopied(false);
     }, 1000);
   };
-
+  console.log(purchases.length);
   return (
     <section className="h-screen flex-d w-100 items-center justify-center">
       <div className="flex-1 flex flex-col lg:space-y-0 lg:flex-row lg:space-x-10 sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
@@ -265,47 +265,69 @@ const UserPage = () => {
                 any questions{" "}
               </p>
 
-              <ul className="flex flex-col items-left mt-3 max-h-72 text-white/50 overflow-y-scroll">
-                {purchases?.map((purchase, index) => {
-                  return (
-                    <li
-                      className="relative text-base text-left text-gray-300 bg-slate-800 mb-2 rounded-md p-3"
-                      key={purchase.id}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-4 h-4 xl:w-5 xl:h-5 absolute right-1 top-1 cursor-pointer"
-                        onClick={() =>
-                          copyPurchaseId(purchase.payment_id, index)
-                        }
+              {purchases.length > 0 ? (
+                <ul className="flex flex-col items-left mt-3 max-h-72 text-white/50 overflow-y-scroll">
+                  {purchases?.map((purchase, index) => {
+                    return (
+                      <li
+                        className="relative text-base text-left text-gray-300 bg-slate-800 mb-2 rounded-md p-3"
+                        key={purchase.id}
                       >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-                        />
-                      </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          class="w-4 h-4 xl:w-5 xl:h-5 absolute right-1 top-1 cursor-pointer"
+                          onClick={() =>
+                            copyPurchaseId(purchase.payment_id, index)
+                          }
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                          />
+                        </svg>
 
-                      <div className="ml-1 flex flex-row">
-                        <p className="font-bold">Product name:</p>
-                        <p className="ml-1"> {purchase.package_name}</p>
-                      </div>
-                      <div className="ml-1 flex flex-row">
-                        <p className="font-bold">Price:</p>
-                        <p className="ml-1"> {purchase.price}</p>
-                      </div>
-                      <div className="ml-1 flex flex-row">
-                        <p className="font-bold">Purchase id:</p>
-                        <p className="ml-1"> {purchase.payment_id}</p>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
+                        <div className="ml-1 flex flex-row">
+                          <p className="font-bold">Product name:</p>
+                          <p className="ml-1"> {purchase.package_name}</p>
+                        </div>
+                        <div className="ml-1 flex flex-row">
+                          <p className="font-bold">Price:</p>
+                          <p className="ml-1"> {purchase.price}</p>
+                        </div>
+                        <div className="ml-1 flex flex-row">
+                          <p className="font-bold">Purchase id:</p>
+                          <p className="ml-1"> {purchase.payment_id}</p>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              ) : (
+                <div className="flex flex-col items-center mt-4 text-grayWhite">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.0"
+                    stroke="currentColor"
+                    class="w-20 h-20 opacity-75"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+                    />
+                  </svg>
+                  <p className="text-sm mt-3 text-grayWhite">
+                    No purchases yet..
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         </div>
