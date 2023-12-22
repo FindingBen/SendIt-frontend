a59f9ede24d44ede356130f6c67f8018b18c7404
Benjamin <beniagic@gmail.com>
2023-12-22 12:33:20 +0100

    commit

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,16 +1,16 @@
 const prod = {
   url: {
-    BASE_URL: "https://sendit-backend-production.up.railway.app/",
+    BASE_URL: "https://sendit-backend-production.up.railway.app",
     //BASE_URL: "http://localhost:8000/",
   },
 };
 
 const dev = {
   url: {
-    //BASE_URL: "http://localhost:8000",
+    BASE_URL: "http://localhost:8000",
     //BASE_URL:"http://localhost:8080"
-    BASE_URL: "https://sendit-backend-production.up.railway.app",
+    //BASE_URL: "https://sendit-backend-production.up.railway.app",
   },
 };
 
-export const config = process.env.NODE_ENV === "development" ? dev : prod;
+export const config = process.env.REACT_APP_ENV === "development" ? dev : prod;
