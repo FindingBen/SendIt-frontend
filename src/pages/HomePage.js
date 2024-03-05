e250036ef263eff243f3f510cc259afe5e2333e5
FindingBen <beniagic@gmail.com>
2024-03-05 15:39:11 +0100

    commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -354,7 +354,7 @@ const HomePage = () => {
                             )}
                           </div>
 
-                          <div className="flex-1">
+                          <div className="flex flex-row items-center justify-center gap-1">
                             <Link
                               className="hover:bg-sky-300 rounded"
                               type="button"
@@ -377,50 +377,12 @@ const HomePage = () => {
                             </Link>
 
                             {message.status === "sent" ? (
-                              <Link
-                                className="hover:bg-sky-300 rounded disabled-link mx-2"
-                                type="button"
-                                to={`/sms_editor/${message.id}`}
-                                // data-mdb-ripple-color="dark"
-                              >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth="1.5"
-                                  fill="currentColor"
-                                  className="lg:w-6 lg:h-6 w-4 h-4 fill-gray-500"
-                                >
-                                  <path
-                                    d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                  />
-                                </svg>
-                              </Link>
+                              <></>
                             ) : message.status === "Scheduled" ? (
-                              <Link
-                                className="hover:bg-sky-300 rounded disabled-link mx-2"
-                                type="button"
-                                to={`/sms_editor/${message.id}`}
-                                // data-mdb-ripple-color="dark"
-                              >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth="1.5"
-                                  fill="currentColor"
-                                  className="lg:w-6 lg:h-6 w-4 h-4 fill-gray-600"
-                                >
-                                  <path
-                                    d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97zM6.75 8.25a.75.75 0 01.75-.75h9a.75.75 0 010 1.5h-9a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H7.5z"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                  />
-                                </svg>
-                              </Link>
+                              <></>
                             ) : (
                               <Link
-                                className="hover:bg-sky-300 rounded mx-2"
+                                className="hover:bg-sky-300 rounded"
                                 type="button"
                                 to={`/sms_editor/${message.id}`}
 
@@ -443,7 +405,7 @@ const HomePage = () => {
                             )}
                             <button
                               type="button"
-                              className="hover:bg-sky-300 rounded mr-2"
+                              className="hover:bg-sky-300 rounded"
                               onClick={() => duplicateMessage(message.id)}
                             >
                               <svg
