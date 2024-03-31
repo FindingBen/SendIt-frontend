61a52f3692d6edd3609df6bcb601cdf87b62a9f8
FindingBen <beniagic@gmail.com>
2024-03-31 21:22:25 +0200

    commit

--- src/components/Analytics/OverallStatistics.js
+++ src/components/Analytics/OverallStatistics.js
@@ -4,26 +4,10 @@ import { motion } from "framer-motion";
 
 const OverallStatistics = ({ totalValues }) => {
   return (
-    <div className="flex bg-black rounded-2xl p-2 w-[72%] shadow-lg">
-      <div className="flex flex-row items-center lg:w-[275px] rounded-md">
-        <div className="flex-none p-3 my-auto bg-darkBlue rounded-full shadow-lg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="lg:w-8 lg:h-8 w-5 h-5 text-white mx-auto my-auto"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-            />
-          </svg>
-        </div>
+    <div className="flex w-[72%] gap-4">
+      <div className="flex flex-row items-center lg:w-[275px] h-[120px] rounded-2xl bg-black">
         <div className="p-2 flex items-start flex-col rounded-md mx-2 my-auto">
-          <p className="text-white lg:text-normal font-light text-justify">
+          <p className="text-white text-xl lg:text-normal font-light text-justify">
             Total sends
           </p>
           {totalValues ? (
@@ -47,31 +31,9 @@ const OverallStatistics = ({ totalValues }) => {
         </div>
       </div>
 
-      <div className="flex flex-row items-center lg:w-[275px] rounded-md">
-        <div className="flex-none p-3 my-auto bg-darkBlue rounded-full shadow-lg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="lg:w-8 lg:h-8 w-5 h-5 text-white mx-auto my-auto"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-        </div>
-
+      <div className="flex flex-row items-center lg:w-[275px] rounded-2xl bg-black">
         <div className="p-2 flex items-start flex-col rounded-md mx-2 my-auto">
-          <p className="text-white text-normal font-light text-justify">
+          <p className="text-white text-xl font-light text-justify">
             Total Views
           </p>
           {totalValues ? (
@@ -95,25 +57,9 @@ const OverallStatistics = ({ totalValues }) => {
         </div>
       </div>
 
-      <div className="flex flex-row items-center lg:w-[275px] rounded-md">
-        <div className="flex-none p-3 my-auto bg-darkBlue rounded-full shadow-lg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="lg:w-8 lg:h-8 w-5 h-5 text-white mx-auto my-auto"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </div>
+      <div className="flex flex-row items-center lg:w-[275px] rounded-2xl bg-black">
         <div className="p-2 flex items-start flex-col rounded-md mx-2 my-auto">
-          <p className="text-white text-normal font-light text-justify">
+          <p className="text-white text-xl font-light text-justify">
             Bounce Rate
           </p>
           {totalValues ? (
@@ -138,25 +84,9 @@ const OverallStatistics = ({ totalValues }) => {
         </div>
       </div>
 
-      <div className="flex flex-row items-center lg:w-[275px] rounded-md">
-        <div className="flex-none p-3 my-auto bg-darkBlue rounded-full shadow-lg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="lg:w-8 lg:h-8 w-5 h-5 text-white mx-auto my-auto"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z"
-            />
-          </svg>
-        </div>
+      <div className="flex flex-row items-center lg:w-[275px] rounded-2xl bg-black">
         <div className="p-2 flex items-start flex-col rounded-md mx-2 my-auto">
-          <p className="text-white text-normal font-light text-justify">
+          <p className="text-white text-xl font-light text-justify">
             Overall rate
           </p>
           {totalValues ? (
