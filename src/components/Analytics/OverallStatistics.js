c07cd557d0663d35580d9ca6604ee0269203a91e
FindingBen <beniagic@gmail.com>
2024-04-28 20:33:11 +0200

    commit

--- src/components/Analytics/OverallStatistics.js
+++ src/components/Analytics/OverallStatistics.js
@@ -57,32 +57,7 @@ const OverallStatistics = ({ totalValues }) => {
         </div>
       </div>
 
-      <div className="flex flex-row items-center lg:w-[275px] rounded-2xl bg-gradient-to-b from-lighterMainBlue to-mainBlue border-2 border-gray-800 relative">
-        <div className="p-2 flex items-start flex-col rounded-md mx-2 my-auto">
-          {totalValues ? (
-            <motion.div
-              initial={{ opacity: 0, scale: 0.0 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 0.4,
-                delay: 0.1,
-                ease: [0, 0.41, 0.1, 1.01],
-              }}
-              className="text-white text-5xl font-semibold absolute top-5 flex flex-row"
-            >
-              {totalValues.average_bounce_rate ?? 0}
-              <p className="lg:text-xl font-light">%</p>
-            </motion.div>
-          ) : (
-            <p className="text-white font-semibold lg:text-2xl">
-              <SvgLoader width={"w-8"} height={"h-8"} />
-            </p>
-          )}
-          <p className="text-white text-normal font-semibold text-justify absolute right-2 bottom-2">
-            Bounce Rate
-          </p>
-        </div>
-      </div>
+  
 
       <div className="flex flex-row items-center lg:w-[275px] rounded-2xl bg-gradient-to-b from-lighterMainBlue to-mainBlue border-2 border-gray-800 relative">
         <div className="p-2 flex items-start flex-col rounded-md mx-2 my-auto">
--- src/components/Analytics/Statistics.js
+++ src/components/Analytics/Statistics.js
@@ -6,86 +6,83 @@ import ProgressBar from "../Progress/ProgressBar";
 const Statistics = ({ views }) => {
   console.log(views);
   return (
-    <div className="flex flex-col w-full">
-      <p className="text-white text-2xl font-semibold text-justify">
-        Other statistics
-      </p>
-      <div className="p-2 flex flex-col rounded-2xl my-1 relative">
-        <div className="flex flex-col gap-4 p-2">
-          <div className="flex flex-col p-1 relative">
-            <p className="text-white text-normal font-semibold text-justify">
-              Bounce rate
+    <div className="p-4 bg-gradient-to-b from-lighterMainBlue to-mainBlue border-2 border-gray-800 w-full flex flex-col rounded-2xl my-1 relative ">
+      <div className="flex flex-col gap-4 p-2">
+        <div className="flex flex-col p-1 relative">
+          <p className="text-white text-normal font-semibold text-justify">
+            Bounce rate
+          </p>
+          <ProgressBar progress={views?.data.sorted_total_data.bounceRate} />
+          {views ? (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.4,
+                delay: 0.1,
+                ease: [0, 0.41, 0.1, 1.01],
+              }}
+              className="text-white text-normal font-semibold ml-2 absolute right-0"
+            >
+              {views?.data.sorted_total_data.bounceRate} %
+            </motion.div>
+          ) : (
+            <p className="text-white font-semibold absolute right-5">
+              <SvgLoader width={"w-5"} height={"h-5"} />
             </p>
-            <ProgressBar progress={23} />
-            {views ? (
-              <motion.div
-                initial={{ opacity: 0, scale: 0.0 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                  duration: 0.4,
-                  delay: 0.1,
-                  ease: [0, 0.41, 0.1, 1.01],
-                }}
-                className="text-white text-normal font-semibold ml-2 absolute right-0"
-              >
-                {views?.data.sorted_total_data.bounceRate} %
-              </motion.div>
-            ) : (
-              <p className="text-white font-semibold absolute right-5">
-                <SvgLoader width={"w-5"} height={"h-5"} />
-              </p>
-            )}
-          </div>
-          <div className="flex flex-col p-1 relative">
-            <p className="text-white text-normal font-semibold text-justify">
-              Engagement rate
+          )}
+        </div>
+        <div className="flex flex-col p-1 relative">
+          <p className="text-white text-normal font-semibold text-justify">
+            Engagement rate
+          </p>
+          <ProgressBar
+            progress={views?.data.sorted_total_data.engegment_rate_total}
+          />
+          {views ? (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.4,
+                delay: 0.1,
+                ease: [0, 0.41, 0.1, 1.01],
+              }}
+              className="text-white text-normal font-semibold ml-2 absolute right-0"
+            >
+              {views?.data.sorted_total_data.engegment_rate_total} %
+            </motion.div>
+          ) : (
+            <p className="text-white font-semibold absolute right-5">
+              <SvgLoader width={"w-5"} height={"h-5"} />
             </p>
-            <ProgressBar />
-            {views ? (
-              <motion.div
-                initial={{ opacity: 0, scale: 0.0 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                  duration: 0.4,
-                  delay: 0.1,
-                  ease: [0, 0.41, 0.1, 1.01],
-                }}
-                className="text-white text-normal font-semibold ml-2 absolute right-0"
-              >
-                {views?.data.sorted_total_data.engegment_rate_total} %
-              </motion.div>
-            ) : (
-              <p className="text-white font-semibold absolute right-5">
-                <SvgLoader width={"w-5"} height={"h-5"} />
-              </p>
-            )}
-          </div>
+          )}
+        </div>
 
-          <div className="flex flex-col p-1 relative">
-            {" "}
-            <p className="text-white text-normal font-semibold text-justify">
-              Performance
+        <div className="flex flex-col p-1 relative">
+          {" "}
+          <p className="text-white text-normal font-semibold text-justify">
+            Performance
+          </p>
+          <ProgressBar progress={views?.data.overall_perf} />
+          {views ? (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.4,
+                delay: 0.1,
+                ease: [0, 0.41, 0.1, 1.01],
+              }}
+              className="text-white text-normal font-semibold ml-2 absolute right-0"
+            >
+              {views?.data.overall_perf} %
+            </motion.div>
+          ) : (
+            <p className="text-white font-semibold absolute right-5">
+              <SvgLoader width={"w-5"} height={"h-5"} />
             </p>
-            <ProgressBar />
-            {views ? (
-              <motion.div
-                initial={{ opacity: 0, scale: 0.0 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{
-                  duration: 0.4,
-                  delay: 0.1,
-                  ease: [0, 0.41, 0.1, 1.01],
-                }}
-                className="text-white text-normal font-semibold ml-2 absolute -right-5"
-              >
-                {views?.data.overall_perf} %
-              </motion.div>
-            ) : (
-              <p className="text-white font-semibold absolute right-5">
-                <SvgLoader width={"w-5"} height={"h-5"} />
-              </p>
-            )}
-          </div>
+          )}
         </div>
       </div>
     </div>
