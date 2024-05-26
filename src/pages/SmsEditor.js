249da729df0558846cf37e7d513c733915b75891
FindingBen <beniagic@gmail.com>
2024-05-26 13:07:27 +0200

    commit

--- src/pages/SmsEditor.js
+++ src/pages/SmsEditor.js
@@ -26,7 +26,7 @@ const SmsEditor = () => {
   const [istext, setIsText] = useState(false);
   const [isName, setIsname] = useState(false);
   const [isLink, setIslink] = useState(false);
-  const [recipients, setRecipients] = useState();
+  const [recipients, setRecipients] = useState([]);
   const BASE = "https://spp.up.railway.app";
   const BASE_URL = "https://spplane.app";
   const linkURLBase = `${BASE_URL}/view/${params.id}`;
@@ -37,7 +37,7 @@ const SmsEditor = () => {
     getContactLists();
     getUser();
   }, []);
-
+  console.log(recipients);
   useEffect(() => {
     if (!smsText.includes("#Link")) {
       setIslink(false);
@@ -101,7 +101,7 @@ const SmsEditor = () => {
 
   const handleSms = (e) => {
     setSmsText(e.target.value);
-    if (smsText.length > 10) {
+    if (smsText.length > 5) {
       setIsText(true);
     } else {
       setIsText(false);
@@ -169,11 +169,12 @@ const SmsEditor = () => {
     if (e.target.value !== "Choose") {
       const recipientData = JSON.parse(e.target.value);
       setRecipients(recipientData);
-      console.log(recipientData.contact_lenght);
+
       if (recipientData.contact_lenght > 0) {
         setIslist(true);
       } else if (recipientData.contact_lenght === 0) {
         setIslist(false);
+        setRecipients(recipientData);
       }
     } else {
       setIslist(false);
@@ -190,7 +191,7 @@ const SmsEditor = () => {
   const handleDate = (date) => {
     setDateSchedule(date);
   };
-  console.log(recipients.contact_length);
+  console.log(smsText?.length);
   return (
     <section className="min-h-screen w-100 items-center justify-center">
       <div className="flex-1 flex flex-col space-y-5 lg:space-y-0 lg:flex-row">
@@ -204,7 +205,7 @@ const SmsEditor = () => {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-4 rounded-md mx-20">
-            <div className="grid gap-2 bg-mainBlue border-gray-800 border-2 rounded-2xl">
+            <div className="grid gap-2 bg-mainBlue border-gray-800 border-2 rounded-2xl relative">
               <div class="rounded p-10">
                 <h3 class="text-2xl text-left font-extralight text-white mb-4">
                   Sms send
@@ -263,7 +264,7 @@ const SmsEditor = () => {
                       value={smsText}
                     ></textarea>
                   </div>
-                  <div className="grid grid-cols-2">
+                  <div className="grid grid-cols-3">
                     <div className="flex flex-row">
                       <button
                         onClick={() => {
@@ -271,11 +272,11 @@ const SmsEditor = () => {
                         }}
                         type="submit"
                         color="dark"
-                        className="bg-green-800 hover:bg-green-400 text-white font-normal py-2 px-3 rounded text-sm xl:text-base mt-4"
+                        className="bg-purpleHaze hover:bg-purple-700 text-white font-normal py-2 px-3 rounded text-sm xl:text-base mt-4"
                         disabled={
-                          user &&
-                          recipients &&
-                          user.sms_count < recipients.contact_length
+                          recipients.contact_lenght === 0 ||
+                          !recipients ||
+                          smsText.length === 0
                         }
                       >
                         Send
@@ -285,6 +286,11 @@ const SmsEditor = () => {
                         <button
                           onClick={() => setShowSchedule(true)}
                           type="submit"
+                          disabled={
+                            recipients.contact_lenght === 0 ||
+                            !recipients ||
+                            smsText.length === 0
+                          }
                           color="dark"
                           className="bg-blue-700 hover:bg-blue-400 text-white font-normal py-2 px-3 rounded text-sm xl:text-base mt-4 ml-3"
                         >
@@ -297,6 +303,36 @@ const SmsEditor = () => {
                   </div>
                 </div>
               </div>
+              <div className="flex flex-row gap-2 absolute right-2 bottom-2">
+                <div className="flex flex-row">
+                  <button
+                    onClick={handleAddLink}
+                    value={linkURLBase}
+                    disabled={smsText.length === 0 || recipients.length === 0}
+                    className={`${
+                      smsText.length === 0 || recipients.length === 0
+                        ? "bg-gray-500"
+                        : "bg-blue-950 cursor-pointer"
+                    } hover:bg-blue-800 text-white font-semibold py-2 px-3 border-gray-500 rounded text-sm xl:text-base ml-2 mt-4`}
+                  >
+                    #Link
+                  </button>
+                </div>
+                <div className="flex flex-row">
+                  <button
+                    onClick={handleNameLink}
+                    disabled={smsText.length === 0 || !recipients}
+                    className={`${
+                      smsText.length === 0 || !recipients
+                        ? "bg-gray-500"
+                        : "bg-blue-950 cursor-pointer"
+                    } hover:bg-blue-800 text-white font-semibold py-2 px-3 border-gray-500 rounded text-sm xl:text-base ml-2 mt-4`}
+                  >
+                    #FirstName
+                  </button>
+                </div>
+              </div>
+
               {errorMessage && (
                 <div
                   class="mx-5 p-4 mb-4 h-20 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 mt-2"
@@ -340,7 +376,7 @@ const SmsEditor = () => {
               />
               <div className="flex flex-col mt-5 p-2">
                 <div className="h-10">
-                  {recipients.contact_lenght === 0 ? (
+                  {recipients?.contact_lenght === 0 ? (
                     <p className="font-semibold text-sm text-red-500 text-justify">
                       Make sure to select contact list with recipients!
                     </p>
@@ -348,35 +384,27 @@ const SmsEditor = () => {
                     <p></p>
                   )}
                 </div>
-                <div className="flex flex-row gap-2">
-                  <div className="flex flex-row p-2">
-                    <button
-                      onClick={handleAddLink}
-                      value={linkURLBase}
-                      disabled={smsText.length === 0 || !recipients}
-                      className={`${
-                        smsText.length === 0 || !recipients
-                          ? "bg-gray-500"
-                          : "bg-blue-800 cursor-pointer"
-                      } hover:bg-blue-400 text-white font-bold py-2 px-3 rounded text-sm xl:text-base ml-2`}
+                {islist && isLink && isName && istext && (
+                  <div className="flex flex-row gap-2">
+                    <p className="text-green-600 text-normal text-justify">
+                      Yout text is ready for sending!
+                    </p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="size-6 text-green-600"
                     >
-                      #Link
-                    </button>
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
+                      />
+                    </svg>
                   </div>
-                  <div className="flex flex-row p-2">
-                    <button
-                      onClick={handleNameLink}
-                      disabled={smsText.length === 0 || !recipients}
-                      className={`${
-                        smsText.length === 0 || !recipients
-                          ? "bg-gray-500"
-                          : "bg-blue-800 cursor-pointer"
-                      } hover:bg-blue-400 text-white font-bold py-2 px-3 rounded text-sm xl:text-base ml-2`}
-                    >
-                      #FirstName
-                    </button>
-                  </div>
-                </div>
+                )}
               </div>
             </div>
             <SmsConfirmModal
