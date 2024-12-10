0150cbc7ec4c7238080ecd8bce6670b74cb32518
FindingBen <beniagic@gmail.com>
2024-12-10 18:20:38 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -59,8 +59,8 @@ function App() {
       <div className="App">
         <ElementProvider>
           <Routes>
-            {/* <Route path="register" element={<DemoPage />} /> */}
-            <Route path="register" element={<RegisterPage />} />
+            <Route path="register" element={<DemoPage />} />
+            {/* <Route path="register" element={<RegisterPage />} /> */}
             <Route path="login" element={<Login></Login>} />
             <Route
               path="reset_password"
