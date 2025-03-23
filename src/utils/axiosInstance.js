9683d4235e77852203e8f01917f4e7108175baab
FindingBen <beniagic@gmail.com>
2025-03-23 17:31:28 +0100

    commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,74 +2,85 @@ import axios from "axios";
 import jwt_decode from "jwt-decode";
 import dayjs from "dayjs";
 import { setCredentials, logOut } from "../redux/reducers/authSlice";
-import { useRef, useState, useEffect } from "react";
+import { useRef } from "react";
 import { config } from "../constants/Constants";
 import { useRedux } from "../constants/reduxImports";
 import { cleanContactLists } from "../redux/reducers/contactListReducer";
 import { cleanPackage } from "../redux/reducers/packageReducer";
 import { clearMessages } from "../redux/reducers/messageReducer";
-import { getSessionToken } from "@shopify/app-bridge-utils";
-import createApp from "@shopify/app-bridge";
-
-const shopifyConfig = {
-  apiKey: "537514fd5c87b791b2959cfe452a10ea",
-  host: new URLSearchParams(window.location.search).get("host"),
-  forceRedirect: true,
-};
-
-const app = createApp(shopifyConfig);
 
 const useAxiosInstance = () => {
-  const { dispatch } = useRedux();
+  const { currentToken, currentUser, dispatch } = useRedux();
   const baseURL = config.url.BASE_URL;
-  const axiosInstanceRef = useRef(null);
-  const [axiosReady, setAxiosReady] = useState(false);
+
   const createAxiosInstance = (token) => {
-    return axios.create({
+    const instance = axios.create({
       baseURL: baseURL,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
     });
-  };
-  const fetchShopifyToken = async () => {
+
     try {
-      return await getSessionToken(app);
+      if (token) {
+        instance.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${currentToken}`;
+      }
+
+      return instance;
     } catch (error) {
-      console.error("Error fetching session token:", error);
-      return null;
+      dispatch(logOut());
+      dispatch(cleanPackage());
+      dispatch(clearMessages());
+      dispatch(cleanContactLists());
     }
   };
 
-  useEffect(() => {
-    const setupAxios = async () => {
-      const token = await fetchShopifyToken();
-      if (token) {
-        axiosInstanceRef.current = createAxiosInstance(token);
-        setAxiosReady(true);
-      } else {
+  const axiosInstanceRef = useRef(createAxiosInstance(currentToken));
+  const tokenExpiration = dayjs.unix(jwt_decode(currentToken).exp);
+
+  axiosInstanceRef.current.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${currentToken}`;
+
+  axiosInstanceRef.current.interceptors.request.use(async (req) => {
+    const now = dayjs();
+    const timeUntilExpiration = tokenExpiration.diff(now, "seconds");
+
+    if (timeUntilExpiration < 1 && !req.isTokenRefresh) {
+      // Refresh the token if it's about to expire in 5 minutes or less
+      const getRefreshToken = localStorage.getItem("refreshToken");
+
+      try {
+        const response = await axios.post(`${baseURL}/api/token/refresh/`, {
+          refresh: getRefreshToken,
+        });
+
+        const newToken = response.data.access;
+        localStorage.setItem("refreshToken", response.data.refresh);
+
+        dispatch(setCredentials({ ...response.data, currentUser }));
+
+        const newAxiosInstance = createAxiosInstance(newToken);
+        axiosInstanceRef.current = newAxiosInstance;
+
+        axiosInstanceRef.current.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${newToken}`;
+        //dispatch(setCredentials({ user, access: newToken }));
+
+        req.headers.Authorization = `Bearer ${newToken}`;
+        req.isTokenRefresh = true;
+      } catch (error) {
+        localStorage.removeItem("refreshToken");
         dispatch(logOut());
         dispatch(cleanContactLists());
         dispatch(cleanPackage());
         dispatch(clearMessages());
+        throw error;
       }
-    };
+    }
 
-    setupAxios();
-  }, []);
-
-
-  useEffect(() => {
-    if (!axiosInstanceRef.current) return;
-
-    axiosInstanceRef.current.interceptors.request.use(async (req) => {
-      const token = await fetchShopifyToken();
-      if (token) {
-        req.headers.Authorization = `Bearer ${token}`;
-      }
-      return req;
-    });
-  }, [axiosReady]);
+    return req;
+  });
 
   return axiosInstanceRef.current;
 };
