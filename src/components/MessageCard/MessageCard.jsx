2a7433b1e086c1de5059bfb9651d13fd5078e592
FindingBen <beniagic@gmail.com>
2025-06-05 15:23:29 +0200

    commit

--- src/components/MessageCard/MessageCard.jsx
+++ src/components/MessageCard/MessageCard.jsx
@@ -99,24 +99,28 @@ export const MessageCard = ({
             </svg>
           </Link>
         </div>
-        <div className="border-gray-800 rounded-md mx-auto my-auto p-0.5 hover:bg-cyan-500  cursor-pointer transition ease-in-out delay-90 hover:-translate-y-1 hover:scale-105">
-          <Link type="button" className="" to={`/sending_flow/${message.id}`}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="lg:w-5 lg:h-5 w-4 h-4 2xl:w-7 2xl:h-8 fill-gray-200 text-black"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
-              />
-            </svg>
-          </Link>
-        </div>
+        {message.status === "Draft" ? (
+          <div className="border-gray-800 rounded-md mx-auto my-auto p-0.5 hover:bg-cyan-500  cursor-pointer transition ease-in-out delay-90 hover:-translate-y-1 hover:scale-105">
+            <Link type="button" className="" to={`/sending_flow/${message.id}`}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="lg:w-5 lg:h-5 w-4 h-4 2xl:w-7 2xl:h-8 fill-gray-200 text-black"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                />
+              </svg>
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
         <div className="relative border-gray-800 rounded-md mx-auto my-auto p-0.5 hover:bg-cyan-500 cursor-pointer transition ease-in-out delay-90 hover:-translate-y-1 hover:scale-105">
           <button
             id="dropdownDefaultButton"
