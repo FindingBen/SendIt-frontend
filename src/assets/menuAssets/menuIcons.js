17ae4b24ffd2db88f4ba9393fa51fa2b1b4d0986
Benjamin <beniagic@gmail.com>
2025-10-26 19:09:34 +0100

    commit

--- src/assets/menuAssets/menuIcons.js
+++ src/assets/menuAssets/menuIcons.js
@@ -37,14 +37,14 @@ export const menu = [
     title: "Billing",
     location: "/purchase_history",
   },
+  // {
+  //   id: 6,
+  //   element: <ProductsIcon />,
+  //   title: "Products",
+  //   location: "/products_shopify/",
+  // },
   {
     id: 6,
-    element: <ProductsIcon />,
-    title: "Products",
-    location: "/products_shopify/",
-  },
-  {
-    id: 7,
     element: <ArchiveIcon />,
     title: "Archives",
     location: "/archive/",
